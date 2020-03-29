f8e232f27cb8b72f262c7fb27f3656499b798a59
Dan Foley <dan@dan-foley.com>
2020-03-29 20:47:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,3 +10,9 @@ class Application
   end
 
 end
+
+
+# If it's before noon, greet the user with "Good Morning!"
+#
+# If it's after noon, greet the user with "Good Afternoon!".
+# Remember that getting the time may return results in 24 hour time! Anytime on or after 12 is the afternoon.
